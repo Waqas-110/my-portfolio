611ab9b3b48e2bcd4139878481f40abd49917b9a
Waqas-110 <waqasanwarmagsi5@gmail.com>
2025-06-08 13:45:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def contact():
     return redirect("/")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
 
 
